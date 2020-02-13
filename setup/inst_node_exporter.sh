df5b5d400a6ba839a26d8196a665cb0399bec00d
Adam Bihari <adam.b@securithings.com>
2020-02-13 11:52:38 +0200

    Update node_exporter with inline systemd file

--- setup/inst_node_exporter.sh
+++ setup/inst_node_exporter.sh
@@ -2,7 +2,6 @@
 
 # This script downloads and installs node_exporter of the requested version on a host.
 # node_exporter is set up as a systemd service, which requiers a daemon-reload.
-# INSTRUCTIONS: Before running make sure node_exporter.service exists in same dir.
 # BLAME: DamDam (Adam Bihari)
 
 node_exporter_ver="0.18.0"
@@ -23,7 +22,22 @@ sudo mkdir -p /var/lib/node_exporter/textfile_collector
 sudo chown node_exporter:node_exporter /var/lib/node_exporter
 sudo chown node_exporter:node_exporter /var/lib/node_exporter/textfile_collector
 
-sudo cp ./node_exporter.service /etc/systemd/system/node_exporter.service
+sudo tee /etc/systemd/system/node_exporter.service &>/dev/null << EOF
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter --collector.textfile.directory /var/lib/node_exporter/textfile_collector \
+ --no-collector.infiniband
+
+[Install]
+WantedBy=multi-user.target
+EOF
 
 rm -rf /tmp/node_exporter-$node_exporter_ver.linux-amd64.tar.gz \
   ./node_exporter-$node_exporter_ver.linux-amd64
